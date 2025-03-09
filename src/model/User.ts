0842e609742ac298cd1594dcab0c235199bfea6f
rajaNayak123 <nayakraja151@gmail.com>
2025-03-09 10:06:43 +0530

    Create user model and validate the fields using zod

---
+++ src/model/User.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// we use typescript and we connect with Mongoose so we store the data in document formates
+// Message structure and schema
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const messageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+export interface User extends Document {
+  username: String;
+  email: String;
+  password: String;
+  verifyCode: String;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const userSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    trim: true,
+    unique: true,
+    match: [/.+\@.+\..+/, "write valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    select: false,
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "verifyCode is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "verifyCodeExpiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [messageSchema],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", userSchema); // when first time schema created
+
+export default UserModel;
